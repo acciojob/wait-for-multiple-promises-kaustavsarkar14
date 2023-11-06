a3ff53355d0a85718e134c685ce6c548334acfad
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-11-06 11:53:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 //your JS code here. If required.
+const table = document.querySelector('.table')
+const p1 = new Promise((res)=>{
+	setTimeout(()=>{
+		res(["Promise 1",1000])
+	},1000)
+})
+const p2 = new Promise((res)=>{
+	setTimeout(()=>{
+		res(["Promise 2",2000])
+	},2000)
+})
+const p3 = new Promise((res)=>{
+	setTimeout(()=>{
+		res(["Promise 3",3000])
+	},3000)
+})
+
+async function addTableRow() {
+	Promise.all([p1, p2, p3]).then((resolvedData)=>{
+		resolvedData.map(data=>{
+			appendData(data[0], data[1])
+		})
+	appendData("Total", 3000)
+	}) 
+	
+}
+
+addTableRow()
+
+function appendData(promiseName, promiseTime) {
+	const name = document.createElement('td')
+	const time = document.createElement('td')
+	const tr = document.createElement('tr')
+	name.innerText = promiseName
+	time.innerText = promiseTime
+	tr.appendChild(name)
+	tr.appendChild(time)
+	table.appendChild(tr)
+}
